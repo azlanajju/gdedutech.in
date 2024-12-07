393885b512a2ce64a6bba87f52b8f11c2093a0ee
Thameem-20 <ahmedthameem20@gmail.com>
2024-12-07 06:42:28 +0530

    try admin login

--- adminPanel/admin_login.php
+++ adminPanel/admin_login.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../Configurations/config.php'; // Include database configuration
-require_once __DIR__ . '../vendor/autoload.php'; // Load Composer dependencies (including JWT)
+require_once __DIR__ . '/../vendor/autoload.php'; // Load Composer dependencies (including JWT)
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
