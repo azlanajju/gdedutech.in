b3af7ed840f201aec456d5963c42dd56d76939b6
Thameem-20 <ahmedthameem20@gmail.com>
2024-12-07 06:55:40 +0530

    fixed admin config

--- Configurations/config.php
+++ Configurations/config.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-define('UPLOADS_DIR', __DIR__ . '/uploads/');
+// define('UPLOADS_DIR', __DIR__ . '/uploads/');
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
