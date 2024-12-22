0ae9deda39c3db480abf71254879fe93004cc5b0
Thameem-20 <ahmedthameem20@gmail.com>
2024-12-22 10:55:00 +0530

    updated

--- studentPanel/Profile/index.php
+++ studentPanel/Profile/index.php
@@ -180,7 +180,7 @@ $courses_result = $courses_stmt->get_result();
                             </a>
                         </li>
                         <li class="w-100">
-                            <a href="./Resources/index.php" class="nav-link text-white">
+                            <a href="../Resources/index.php" class="nav-link text-white">
                                 <i class="bi bi-file-earmark-text me-2"></i> Resources
                             </a>
                         </li>
