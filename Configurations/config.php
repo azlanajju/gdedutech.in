a2dd300a49f2d3ea0fceecaf539d6f85efa90a7f
Azlan <98301786+azlanajju@users.noreply.github.com>
2025-06-03 09:43:22 +0530

    Update config.php

--- Configurations/config.php
+++ Configurations/config.php
@@ -4,21 +4,17 @@ $host = $_SERVER['HTTP_HOST'];
 if (strpos($host, 'gdedutech.com') !== false) {
     $conn = new mysqli("srv1752.hstgr.io", "u229215627_edutech", "Azl@n2002", "u229215627_edutech");
 
-} elseif (strpos($host, 'edutech.intelexsolutions-test.site') !== false || 
-          strpos($host, 'gdedutech.in') !== false) {
-    $conn = new mysqli("localhost", "u593219986_edutech", "GdEdutech@1234", "u593219986_edutech");
-
 } else {
     $conn = new mysqli("localhost", "root", "", "gd_edu_tech");
 }
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
 // define('UPLOADS_DIR', __DIR__ . '/uploads/');
 $adminMail="gdedutech24@gmail.com";
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
 
 ?>
